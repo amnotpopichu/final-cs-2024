429297502f9d60459dcca8f6d3ce49115319b575
Leo <153849409+amnotpopichu@users.noreply.github.com>
2024-09-26 13:45:07 -0700

    e

---
+++ geomapplot.py
@@ -0,0 +1,22 @@
+import plotly.express as px
+import pandas as pd
+
+
+# Import data from USGS
+data = pd.read_csv('income.csv')
+
+
+# Drop rows with missing or invalid values in the 'mag' column
+data = data.dropna(subset=['Lat'])
+data = data.dropna(subset=['Lon'])
+data = data.dropna(subset=['Median'])
+data = data.dropna(subset=['City'])
+data = data[data.Median >= 100000]
+
+
+# Create scatter map
+fig = px.scatter_geo(data, lat='Lat', lon='Lon',
+                     hover_name='City', size="Median",
+                     title='Median Income in US')
+#fig = px.scatter(data, x="Height",y="Weight", color = "Alignment", hover_name = "name", title='Random superhero dataset that angi gave us')
+fig.show()
